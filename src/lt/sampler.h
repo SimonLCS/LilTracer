27ea1c17a5a57cdd863d0185af8cda6666023d1f
SimonLCS <lucas.sl@free.fr>
2024-11-25 20:05:44 +0100

    sampler

--- src/lt/sampler.h
+++ src/lt/sampler.h
@@ -16,31 +16,64 @@ namespace LT_NAMESPACE {
 class Sampler {
 public:
     /**
-     * @brief Default constructor.
-     * Initializes the random number generator and uniform distribution.
-     */
+        * @brief Default constructor.
+        * Initializes the random number generator and uniform distribution.
+        */
     Sampler()
     {
-        _rng = std::mt19937(_dev());
-        _urd = std::uniform_real_distribution<Float>(0, 1);
+        s = 0;
     };
 
     /**
-     * @brief Generate a random float.
-     * @return A random float.
-     */
-    Float next_float() { return _urd(_rng); }
+        * @brief Generate a random float.
+        * @return A random float.
+        */
+    Float next_float() { hash(); return s  * (1.0 / Float(0xffffffffu)); }
 
+    void hash() {
+        uint32_t state = s * 747796405u + 2891336453u;
+        uint32_t word = ((state >> ((state >> 28u) + 4u)) ^ state) * 277803737u;
+        s = (word >> 22u) ^ word;
+    }
     /**
-     * @brief Change the seed of the random number generator.
-     * @param s The seed value.
-     */
-    void seed(uint32_t s) { _rng.seed(s); }
+        * @brief Change the seed of the random number generator.
+        * @param s The seed value.
+        */
+    void seed(uint32_t s_) { s = s_; }
 
 private:
-    std::random_device _dev; /**< Random device for seeding. */
-    std::mt19937 _rng; /**< Mersenne Twister random number generator. */
-    std::uniform_real_distribution<Float> _urd; /**< Uniform real distribution. */
+    uint32_t s;
 };
+
+
+//class Sampler {
+//public:
+//    /**
+//     * @brief Default constructor.
+//     * Initializes the random number generator and uniform distribution.
+//     */
+//    Sampler()
+//    {
+//        _rng = std::mt19937(_dev());
+//        _urd = std::uniform_real_distribution<Float>(0, 1);
+//    };
+//
+//    /**
+//     * @brief Generate a random float.
+//     * @return A random float.
+//     */
+//    Float next_float() { return _urd(_rng); }
+//
+//    /**
+//     * @brief Change the seed of the random number generator.
+//     * @param s The seed value.
+//     */
+//    void seed(uint32_t s) { _rng.seed(s); }
+//
+//private:
+//    std::random_device _dev; /**< Random device for seeding. */
+//    std::mt19937 _rng; /**< Mersenne Twister random number generator. */
+//    std::uniform_real_distribution<Float> _urd; /**< Uniform real distribution. */
+//};
 
 } // namespace LT_NAMESPACE
